87435ce76d5d58c916b2ca0f823158285f269fc8
Matteo Emanuele <myprivate@email.it>
2017-03-06 17:03:42 +0100

    Databinding test

--- uberfire-showcase/uberfire-webapp/src/main/java/org/uberfire/client/screens/mylovelyscreen/MyLovelyScreen.java
+++ uberfire-showcase/uberfire-webapp/src/main/java/org/uberfire/client/screens/mylovelyscreen/MyLovelyScreen.java
@@ -1,15 +1,25 @@
 package org.uberfire.client.screens.mylovelyscreen;
 
+import com.google.gwt.user.client.ui.TextBox;
 import org.jboss.errai.common.client.api.Caller;
+import org.jboss.errai.common.client.dom.Button;
+import org.jboss.errai.common.client.dom.Event;
 import org.jboss.errai.security.shared.api.identity.User;
+import org.jboss.errai.ui.shared.api.annotations.DataField;
+import org.jboss.errai.ui.shared.api.annotations.EventHandler;
 import org.uberfire.client.annotations.WorkbenchPartTitle;
 import org.uberfire.client.annotations.WorkbenchPartView;
 import org.uberfire.client.annotations.WorkbenchScreen;
 import org.uberfire.client.mvp.UberElement;
-import org.uberfire.impl.EchoServiceImpl;
+import org.uberfire.shared.EchoService;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.awt.*;
 
 /**
  * Created by matteo on 03/03/17.
@@ -17,15 +27,14 @@ import javax.inject.Inject;
 @WorkbenchScreen( identifier = "MyLovelyScreen" )
 public class MyLovelyScreen {
 
+    private static final Logger logger = LoggerFactory.getLogger(MyLovelyScreen.class);
+
     public interface View extends UberElement<MyLovelyScreen> {
         void setup(String username);
     }
 
-//    @Inject
-//    private Caller<EchoServiceImpl> echoService;
-
     @Inject
-    private EchoServiceImpl echoServiceImpl;
+    private Caller<EchoService> echoService;
 
     @Inject
     private View view;
@@ -33,10 +42,14 @@ public class MyLovelyScreen {
     @Inject
     private User user;
 
+
     @PostConstruct
     public void setup() {
         view.init( this );
         view.setup( user.getIdentifier() );
+        logger.info("!!!");
+        logger.info("!!!");
+        logger.info("!!!");
     }
 
     @WorkbenchPartTitle
--- uberfire-showcase/uberfire-webapp/src/main/java/org/uberfire/client/screens/mylovelyscreen/MyLovelyView.java
+++ uberfire-showcase/uberfire-webapp/src/main/java/org/uberfire/client/screens/mylovelyscreen/MyLovelyView.java
@@ -1,12 +1,14 @@
 package org.uberfire.client.screens.mylovelyscreen;
 
-import org.jboss.errai.common.client.dom.Button;
-import org.jboss.errai.common.client.dom.Heading;
-import org.jboss.errai.common.client.dom.TextInput;
-import org.jboss.errai.common.client.dom.Window;
+import com.google.gwt.user.client.ui.TextBox;
+import org.jboss.errai.common.client.dom.*;
 import org.jboss.errai.ui.client.local.api.IsElement;
 import org.jboss.errai.ui.shared.api.annotations.DataField;
+import org.jboss.errai.ui.shared.api.annotations.EventHandler;
+import org.jboss.errai.ui.shared.api.annotations.ForEvent;
 import org.jboss.errai.ui.shared.api.annotations.Templated;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -17,6 +19,8 @@ import javax.inject.Named;
 @Templated
 public class MyLovelyView implements MyLovelyScreen.View, IsElement {
 
+    private static final Logger logger = LoggerFactory.getLogger(MyLovelyView.class);
+
     private MyLovelyScreen presenter;
 
     @Named( "h1" )
@@ -25,16 +29,14 @@ public class MyLovelyView implements MyLovelyScreen.View, IsElement {
     private Heading welcome;
 
     // The name to reference in the template can also be specified manually
-
-//    @Inject
-//    @DataField
-//    private TextInput echoField;
+    @Inject
+    @DataField
+    private TextBox echoField;
     
     // We can also choose to instantiate our own data fields. Injection is not required.
 
-//    @DataField
-//    private Button submit = (Button) Window.getDocument().createElement("button");
-
+    @DataField("submit")
+    private Button submit = (Button) Window.getDocument().createElement("submit");
 
     @Override
     public void setup(String username) {
@@ -44,5 +46,12 @@ public class MyLovelyView implements MyLovelyScreen.View, IsElement {
     @Override
     public void init(MyLovelyScreen presenter) {
         this.presenter = presenter;
+    }
+
+    @EventHandler("submit")
+    public void doSomething(@ForEvent("click") Event event) {
+        logger.error("****");
+        logger.error("**** " + echoField);
+        logger.error("****");
     }
 }
