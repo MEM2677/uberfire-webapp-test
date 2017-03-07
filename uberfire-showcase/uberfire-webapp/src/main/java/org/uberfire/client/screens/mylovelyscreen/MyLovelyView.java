89e96368689ba992a8eea40f4e082d589eba2222
Matteo Emanuele <myprivate@email.it>
2017-03-07 20:41:34 +0100

    Fixed event handler

--- uberfire-showcase/uberfire-webapp/src/main/java/org/uberfire/client/screens/mylovelyscreen/MyLovelyView.java
+++ uberfire-showcase/uberfire-webapp/src/main/java/org/uberfire/client/screens/mylovelyscreen/MyLovelyView.java
@@ -1,12 +1,12 @@
 package org.uberfire.client.screens.mylovelyscreen;
 
-import com.google.gwt.user.client.ui.TextBox;
-import org.jboss.errai.common.client.dom.*;
+import com.google.gwt.event.dom.client.ClickEvent;
+import org.gwtbootstrap3.client.ui.Button;
+//import org.gwtbootstrap3.client.ui.Heading;
+import org.gwtbootstrap3.client.ui.TextBox;
+import org.jboss.errai.common.client.dom.Heading;
 import org.jboss.errai.ui.client.local.api.IsElement;
-import org.jboss.errai.ui.shared.api.annotations.DataField;
-import org.jboss.errai.ui.shared.api.annotations.EventHandler;
-import org.jboss.errai.ui.shared.api.annotations.ForEvent;
-import org.jboss.errai.ui.shared.api.annotations.Templated;
+import org.jboss.errai.ui.shared.api.annotations.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,15 +32,18 @@ public class MyLovelyView implements MyLovelyScreen.View, IsElement {
     @Inject
     @DataField
     private TextBox echoField;
-    
+
     // We can also choose to instantiate our own data fields. Injection is not required.
 
+    @Inject
     @DataField("submit")
-    private Button submit = (Button) Window.getDocument().createElement("submit");
+//    private Button submit = (Button) Window.getDocument().createElement("submit");
+    private Button submit;
 
     @Override
     public void setup(String username) {
         welcome.setInnerHTML("Welcome " + username + "!");
+        //welcome.setText("Welcome " + username + "!");
     }
 
     @Override
@@ -49,9 +52,11 @@ public class MyLovelyView implements MyLovelyScreen.View, IsElement {
     }
 
     @EventHandler("submit")
-    public void doSomething(@ForEvent("click") Event event) {
+    public void submit(final ClickEvent event) {
         logger.error("****");
-        logger.error("**** " + echoField);
         logger.error("****");
+        logger.error("****");
+        welcome.setInnerHTML("WOW!");
+        //welcome.setText("GOTCHA!");
     }
 }
